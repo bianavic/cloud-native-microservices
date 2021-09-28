befee562ffff4ecf5b568aff4d4b9d15930787f5
bianavic <bianavic@gmail.com>
2021-09-28 20:35:54 -0300

    gateway instance registered to eureka dashboard

--- gateway/src/main/kotlin/com/microservices/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/microservices/gateway/GatewayApplication.kt
@@ -2,7 +2,9 @@ package com.microservices.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+@EnableZuulProxy
 @SpringBootApplication
 class GatewayApplication
 
