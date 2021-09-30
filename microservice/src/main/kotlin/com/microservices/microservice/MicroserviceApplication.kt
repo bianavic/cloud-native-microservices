91f43370dbc702f2c179d2b1873c9905ad00f115
bianavic <bianavic@gmail.com>
2021-09-29 21:19:09 -0300

    fixed dependency incompatibility

--- microservice/src/main/kotlin/com/microservices/microservice/MicroserviceApplication.kt
+++ microservice/src/main/kotlin/com/microservices/microservice/MicroserviceApplication.kt
@@ -2,10 +2,10 @@ package com.microservices.microservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
-@EnableEurekaClient
 class MicroserviceApplication
 
 fun main(args: Array<String>) {
